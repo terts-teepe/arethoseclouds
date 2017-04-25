b1f77565a4f4370fdaedd86122c47dd8ceec6de7
Rishi Speets <rishispeets@gmail.com>
2017-04-25 17:17:26 +0200

    make API call and store data

--- script/script.js
+++ script/script.js
@@ -3,28 +3,29 @@
 - I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
 - I can push a button to toggle between Fahrenheit and Celsius.
 */
-// THIS IS THE API CALL http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={40fa0a20c108fc69e5a2dd3b842723a1}
+// THIS IS THE API CALL 
 
 // https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB6a3yMgNDY1UQnM7O9Gxpn427zFYH1fL0 Google API call 
- 
+ // http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=40fa0a20c108fc69e5a2dd3b842723a1
 
+var lat = 0;
+var lon = 0;
+var acc = 0;
+var weatherCall = "";
+var userLocData = {};
 
-
- 
-
-// Get and store user location  
-
-var latitude = 0;
-var longitude = 0;
-var accuracy = 0;
-
+$(document).ready(function(){
  navigator.geolocation.getCurrentPosition(function(location) {
-  latitude = location.coords.latitude;
-  longitude = location.coords.longitude;
-  accuracy = location.coords.accuracy;
+  lat = location.coords.latitude;
+  lon = location.coords.longitude;
+  acc = location.coords.accuracy;
 });
 
-// Make 1 API call and give location as argument
+// Make 1 API call and give lat/lon as argument
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=40fa0a20c108fc69e5a2dd3b842723a1", function(data){
+    userLocData = (data);
+  });
+});
 
 // Store and display weather data
 
