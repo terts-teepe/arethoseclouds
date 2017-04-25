baf24398931e21371c1fe4beb31f8d2c3ebf9914
Rishi Speets <rishispeets@gmail.com>
2017-04-25 12:35:49 +0200

    add skeleton framework for styling

---
+++ script/script.js
@@ -0,0 +1,18 @@
+/*
+- I can see the weather in my current location.
+- I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
+- I can push a button to toggle between Fahrenheit and Celsius.
+*/
+
+// Get and store user location  
+
+// Make 1 API call and give location as argument
+
+// Store and display weather data
+
+// Change background according to weather
+
+// Button switch between Fahrenheit and Celsius 
+
+// THIS IS THE API CALL http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={40fa0a20c108fc69e5a2dd3b842723a1}
+
