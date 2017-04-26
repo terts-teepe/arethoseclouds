1d3a1418a75e6184fb3090fa1c63ba0bd928b959
Rishi Speets <rishispeets@gmail.com>
2017-04-26 13:32:43 +0200

    jquery to switch bg

--- script/script.js
+++ script/script.js
@@ -1,36 +1,59 @@
-/*
-- I can see the weather in my current location.
-- I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
-- I can push a button to toggle between Fahrenheit and Celsius.
-*/
-// THIS IS THE API CALL 
-
-// https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB6a3yMgNDY1UQnM7O9Gxpn427zFYH1fL0 Google API call 
- // http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=40fa0a20c108fc69e5a2dd3b842723a1
-
 var lat = 0;
 var lon = 0;
 var acc = 0;
 var weatherCall = "";
 var userLocData = {};
 
-$(document).ready(function(){
- navigator.geolocation.getCurrentPosition(function(location) {
-  lat = location.coords.latitude;
-  lon = location.coords.longitude;
-  acc = location.coords.accuracy;
-});
+function changeBg(locParam) {
+  switch (userLocData.weather[0].description) {
+    case "clear sky":
+        $('html').css("background-image", "url(../arethoseclouds/images/clear-sky-japan.jpg)");  
+        break;
+    case "few clouds":
+        $('html').css("background-image", "url(../arethoseclouds/images/few-clouds.jpg)");  
+        break;
+    case "scattered clouds":
+        $('html').css("background-image", "url(../arethoseclouds/images/scattered-clouds.jpg)");  
+        break;
+    case "broken clouds":
+        $('html').css("background-image", "url(../arethoseclouds/images/broken-clouds.jpg)");  
+        break;
+    case "shower rain":
+        $('html').css("background-image", "url(../arethoseclouds/images/rain-shower.jpg)");  
+        break;
+    case "rain":
+        $('html').css("background-image", "url(../arethoseclouds/images/rain.jpg)");  
+        break;
+    case "thunderstorm":
+        $('html').css("background-image", "url(../arethoseclouds/images/thunderstorm.jpg)");  
+        break;
+    case "snow":
+        $('html').css("background-image", "url(../arethoseclouds/images/snow.jpg)");  
+        break;
+    case "mist":
+        $('html').css("background-image", "url(../arethoseclouds/images/mist.jpg)");  
+        break;
+  }
+}
 
-// Make 1 API call and give lat/lon as argument
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=40fa0a20c108fc69e5a2dd3b842723a1", function(data){
-    userLocData = (data);
+$(document).ready(function () {
+  navigator.geolocation.getCurrentPosition(function (location) {
+    lat = location.coords.latitude;
+    lon = location.coords.longitude;
+    acc = location.coords.accuracy;
+  });
+
+  // Make 1 API call and give lat/lon as argument
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=40fa0a20c108fc69e5a2dd3b842723a1", function (data) {
+    userLocData = data;
+    changeBg(userLocData);
   });
 });
 
-// Store and display weather data
 
-// Change background according to weather
-
-// Button switch between Fahrenheit and Celsius 
-
-
+/*
+function changeButton() {
+    document.getElementById('quote').innerHTML = aQuote();
+    document.getElementById('quoteAuthor').innerHTML = aQuoteAuthor();
+};
+*/
