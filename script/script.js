e4d750e4e4754f8262ba8cc52cc550ebb2e2f9b1
Rishi Speets <rishispeets@gmail.com>
2017-04-26 17:35:52 +0200

    added changing background

--- script/script.js
+++ script/script.js
@@ -1,41 +1,58 @@
 var lat = 0;
 var lon = 0;
 var acc = 0;
+var temp = 0;
 var weatherCall = "";
 var userLocData = {};
-
 function changeBg(locParam) {
   switch (userLocData.weather[0].description) {
     case "clear sky":
         $('html').css("background-image", "url(../arethoseclouds/images/clear-sky-japan.jpg)");  
+        $('.weathericon').attr("src","../arethoseclouds/images/icons/clearskyicon.png");
+        $('.temp').text(temp + " degrees celcius"); 
         break;
     case "few clouds":
-        $('html').css("background-image", "url(../arethoseclouds/images/few-clouds.jpg)");  
+        $('html').css("background-image", "url(../arethoseclouds/images/few-clouds.jpg)");
+        $('.weathericon').attr("src","../arethoseclouds/images/icons/few-clouds.png");
+        $('.temp').text(temp + " degrees celcius");   
         break;
-    case "scattered clouds":
-        $('html').css("background-image", "url(../arethoseclouds/images/scattered-clouds.jpg)");  
+    case "overcast clouds":
+        $('html').css("background-image", "url(../arethoseclouds/images/scattered-clouds.jpg)"); 
+        $('.weathericon').attr("src","../arethoseclouds/images/icons/scatteredcloudicon.png");
+        $('.temp').text(temp + " degrees celcius"); 
         break;
     case "broken clouds":
-        $('html').css("background-image", "url(../arethoseclouds/images/broken-clouds.jpg)");  
+        $('html').css("background-image", "url(../arethoseclouds/images/broken-clouds.jpg)"); 
+        $('.weathericon').attr("src","../arethoseclouds/images/icons/brokencloudicon.png");
+        $('.temp').text(temp + " degrees celcius"); 
         break;
     case "shower rain":
-        $('html').css("background-image", "url(../arethoseclouds/images/rain-shower.jpg)");  
+        $('html').css("background-image", "url(../arethoseclouds/images/rain-shower.jpg)"); 
+        $('.weathericon').attr("src","../arethoseclouds/images/icons/rainshowericon.png");
+        $('.temp').text(temp + " degrees celcius");  
         break;
     case "rain":
-        $('html').css("background-image", "url(../arethoseclouds/images/rain.jpg)");  
+        $('html').css("background-image", "url(../arethoseclouds/images/rain.jpg)");
+        $('.weathericon').attr("src","../arethoseclouds/images/icons/rainicon.png");
+        $('.temp').text(temp + " degrees celcius");   
         break;
     case "thunderstorm":
-        $('html').css("background-image", "url(../arethoseclouds/images/thunderstorm.jpg)");  
+        $('html').css("background-image", "url(../arethoseclouds/images/thunderstorm.jpg)");
+        $('.weathericon').attr("src","../arethoseclouds/images/icons/thunderstormicon.png");
+        $('.temp').text(temp + " degrees celcius");   
         break;
     case "snow":
-        $('html').css("background-image", "url(../arethoseclouds/images/snow.jpg)");  
+        $('html').css("background-image", "url(../arethoseclouds/images/snow.jpg)");
+        $('.weathericon').attr("src","../arethoseclouds/images/icons/snowicon.png");
+        $('.temp').text(temp + " degrees celcius");   
         break;
     case "mist":
-        $('html').css("background-image", "url(../arethoseclouds/images/mist.jpg)");  
+        $('html').css("background-image", "url(../arethoseclouds/images/mist.jpg)");
+        $('.weathericon').attr("src","../arethoseclouds/images/icons/misticon.png");
+        $('.temp').text(temp + " degrees celcius");   
         break;
   }
 }
-
 $(document).ready(function () {
   navigator.geolocation.getCurrentPosition(function (location) {
     lat = location.coords.latitude;
@@ -47,9 +64,9 @@ $(document).ready(function () {
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=40fa0a20c108fc69e5a2dd3b842723a1", function (data) {
     userLocData = data;
     changeBg(userLocData);
+    temp = 0;
   });
 });
-
 
 /*
 function changeButton() {
